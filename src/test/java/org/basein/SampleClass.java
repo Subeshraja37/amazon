02e9b96dfe5dc02485094528c252e236e5df7568
Subeshraja37 <subeshraja37@gmail.com>
2023-01-21 13:01:32 +0530

    second time

---
+++ src/test/java/org/basein/SampleClass.java
@@ -0,0 +1,17 @@
+package org.basein;
+
+public class SampleClass {
+private void method1() {
+	System.out.println("first method");
+
+}
+private void method2() {
+System.out.println("second");
+}
+private void method3() {
+System.out.println("third");
+}
+private void method4() {
+System.out.println("fourth");
+}
+}
